f0c9a9f6acfd01cdce9a60552d74908290a944f0
Cảnh Hoàng <canhhoang@Canhs-MacBook-Pro.local>
2017-08-18 21:58:25 +0700

    ok

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -21,6 +21,8 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+
+    <?= $this->render('@app/views/partials/header.php', []) ?>
     <?php $this->head() ?>
 </head>
 <body>
---
+++ backend/views/partials/header.php
@@ -0,0 +1,28 @@
+<?php 
+use yii\helpers\Url;
+?>
+    <meta content="" name="author" />
+    <!-- BEGIN LAYOUT FIRST STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Oswald:400,300,700" rel="stylesheet" type="text/css" />
+    <!-- END LAYOUT FIRST STYLES -->
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="<?= Url::base(); ?>/theme/css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= Url::base(); ?>/theme/css/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= Url::base(); ?>/theme/css/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= Url::base(); ?>/theme/css/bootstrap/css/todo.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= Url::base(); ?>/theme/css/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="<?= Url::base(); ?>/theme/css/bootstrap/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="<?= Url::base(); ?>/theme/css/bootstrap/css/plugins.min.css" rel="stylesheet" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
+    <!-- BEGIN THEME LAYOUT STYLES -->
+    <link href="<?= Url::base(); ?>/theme/css/bootstrap/css/layout.min.css" rel="stylesheet" type="text/css" /> 
+    <link href="<?= Url::base(); ?>/theme/css/bootstrap/css/custom.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= Url::base(); ?>/theme/css/select2.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+    <link href="<?= Url::base(); ?>/theme/css/my-css.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.css">
+    <!-- END THEME LAYOUT STYLES -->
+    <link rel="shortcut icon" href="favicon.ico" />
